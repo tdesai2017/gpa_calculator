5cf6c8d6f709942d7dbf0ebba40b6a5bfbd89e71
Tushar Desai <tushardesai@Tushars-MacBook-Pro.local>
2020-06-25 22:57:14 -0400

    updated code

--- gpa_calculator.rb
+++ gpa_calculator.rb
@@ -1,22 +1,24 @@
 
-#For me
+#qps --> quality points, gpa_hours --> gpa hours | from "grades" in mynortheastern
+#remainint grade distribution --> array of grades we plan to get
 
-current_qps = 354.671
-current_gpa_hours = 93
+def get_gpa(current_qps: , current_gpa_hours:, remaining_grade_distribution:)
+    #Add more grades if neccessary
+    grade_transitions = {A: 16, Aminus: 14.668, Bplus: 13.332, B: 12}
+    remaining_gpa_hours = remaining_grade_distribution.length * 4
+    sum_of_new_grades = 0
+    remaining_grade_distribution.each {|x| sum_of_new_grades+= grade_transitions[x]}
 
-remaining_gpa_hours = 24 
+    # "+ 4" quality points and "+1" gpa hours for eloquent presenter assuming a get an A --> I removed this
+    print((current_qps + sum_of_new_grades)/(current_gpa_hours + remaining_gpa_hours))
+end
 
 
-grade_transitions = {:A => 16, :Aminus => 14.668, :Bplus =>13.332, :B => 12}
+current_qps = 386.671
+current_gpa_hours = 101.000
+remaining_grade_distribution = [:A, :A, :A, :A]
+get_gpa(current_qps: current_qps, current_gpa_hours: current_gpa_hours, remaining_grade_distribution: remaining_grade_distribution)
 
-remaining_grade_distribution = [:Bplus, :Bplus, :Bplus, :Bplus, :Bplus, :Bplus]
-
-sum_of_new_grades = 0
-
-remaining_grade_distribution.each {|x| sum_of_new_grades+= grade_transitions[x]}
-
-# + 4 quality points and +1 gpa hours for eloquent presenter assuming a get an A
-print((current_qps + sum_of_new_grades + 4)/(current_gpa_hours + remaining_gpa_hours + 1))
 
 
 """
